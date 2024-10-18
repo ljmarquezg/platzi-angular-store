8acd762f932e2bbda227766bf0707f30c9bbeb67
Luis Marquez <luis.marquez@e2ycommerce.com>
2024-10-18 18:47:17 +0200

    27 - Product List

---
+++ store/src/app/domains/products/components/product/product.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product',
+  standalone: true,
+  imports: [],
+  templateUrl: './product.component.html',
+  styleUrl: './product.component.scss'
+})
+export class ProductComponent {
+  img = `https://picsum.photos/640/640?=r${Math.random()}`
+}
