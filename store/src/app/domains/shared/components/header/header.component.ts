9ad5addd5d21d72a3fcbe854c54b47d16f48a32b
Luis Marquez <luis.marquez@e2ycommerce.com>
2024-10-23 20:30:02 +0200

    44 - Router Link Active

--- store/src/app/domains/shared/components/header/header.component.ts
+++ store/src/app/domains/shared/components/header/header.component.ts
@@ -1,11 +1,14 @@
 import { Component, inject, Input, signal, SimpleChanges } from '@angular/core';
 import { CartService } from '../../services/cart.service';
-import { RouterLinkWithHref } from '@angular/router';
+import { RouterLinkActive, RouterLinkWithHref } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterLinkWithHref],
+  imports: [
+    RouterLinkWithHref,
+    RouterLinkActive
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
