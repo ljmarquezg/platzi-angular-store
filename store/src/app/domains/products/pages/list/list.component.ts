1815b964939e2aa0c1d8de528a3fd13998d813d8
Luis Marquez <luis.marquez@e2ycommerce.com>
2024-10-27 20:30:57 +0100

    48 - Category Filter

--- store/src/app/domains/products/pages/list/list.component.ts
+++ store/src/app/domains/products/pages/list/list.component.ts
@@ -1,27 +1,49 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, computed, inject, input, Input, signal, SimpleChanges } from '@angular/core';
 import { state } from '@angular/animations';
 import { ProductComponent } from '@products/components/product/product.component';
 import { Product } from '@shared/module/product.model';
 import { HeaderComponent } from "@shared/components/header/header.component";
 import { CartService } from '@shared/services/cart.service';
 import { ProductService } from '@shared/services/product.service';
+import { CategoryService } from '@shared/services/category.service';
+import { Category } from '@shared/module/category.model';
+import { RouterLinkWithHref } from '@angular/router';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [ProductComponent, HeaderComponent],
+  imports: [
+    ProductComponent,
+    HeaderComponent,
+    RouterLinkWithHref
+  ],
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss'
 })
 export class ListComponent {
   private cartService = inject(CartService);
   private productService = inject(ProductService);
+  private categoryService = inject(CategoryService);
 
+  @Input() category_id?: string;
   products = signal<Product[]>([]);
+  categories = signal<Category[]>([]);
   cart = this.cartService.cart;
 
   ngOnInit() {
-    this.productService.getProducts().subscribe({
+    this.getCategories();
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    this.getProducts();
+  }
+
+  addToCart(product: Product) {
+    this.cartService.addToCart(product);
+  }
+
+  private getProducts() {
+    this.productService.getProducts(this.category_id).subscribe({
       next: (products: Product[]) => {
         const items = products.map(product => ({
           ...product,
@@ -35,7 +57,14 @@ export class ListComponent {
     });
   }
 
-  addToCart(product: Product) {
-    this.cartService.addToCart(product);
+  private getCategories() {
+    this.categoryService.getAll().subscribe({
+      next: (categories: Category[]) => {
+        this.categories.set(categories);
+      },
+      error: (error) => {
+        console.error(error);
+      }
+    });
   }
 }
--- store/src/app/domains/shared/services/product.service.ts
+++ store/src/app/domains/shared/services/product.service.ts
@@ -9,8 +9,12 @@ export class ProductService {
   private http = inject(HttpClient);
   constructor() { }
 
-  getProducts() {
-    return this.http.get<Product[]>('https://api.escuelajs.co/api/v1/products')
+  getProducts(category_id?: string) {
+    const url = new URL('https://api.escuelajs.co/api/v1/products');
+    if(category_id) {
+      url.searchParams.append('categoryId', category_id);
+    }
+    return this.http.get<Product[]>(url.toString());
   }
 
   getOne(id: string) {
